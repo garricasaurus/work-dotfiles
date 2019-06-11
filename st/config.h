fd4b79e73ad9aa6ee0f22ea6fc9c9b4415752ba6
Richard Garai <dargzero@gmail.com>
2019-06-11 19:17:37 +0200

    changed st font

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro for Powerline:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Noto Sans Mono-12";
 static int borderpx = 2;
 
 /*
