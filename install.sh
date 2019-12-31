a85333d8f9602a32272abe5b14bd0bd8ed18cc66
Richard Garai <dargzero@gmail.com>
2019-12-31 21:57:29 +0100

    cleanup and update for new laptop

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ SL=$HOME/sl
 echo performing cleanup...
 rm -rf $HOME/.xinitrc
 rm -rf $HOME/.Xresources
-rm -rf $HOME/.config/nvim
 rm -rf $HOME/.config/fish
 rm -rf $HOME/.config/stig
 rm -rf $HOME/helpers
@@ -19,13 +18,11 @@ rm -rf $SL
 
 echo creating directories...
 mkdir -p $HOME/.config
-mkdir -p $HOME/.config/stig
 
 echo creating configs...
 ln -sf $BASEDIR/xinitrc $HOME/.xinitrc
 ln -sf $BASEDIR/Xresources $HOME/.Xresources
 ln -sf $BASEDIR/helpers $HOME/helpers
-ln -sf $BASEDIR/nvim $HOME/.config/nvim
 ln -sf $BASEDIR/fish $HOME/.config/fish
 ln -sf $BASEDIR/stig $HOME/.config/stig
 
@@ -34,13 +31,14 @@ mkdir -p $SL
 git clone https://git.suckless.org/dwm $SL/dwm
 git clone https://git.suckless.org/dmenu $SL/dmenu
 git clone https://git.suckless.org/st $SL/st
-git clone https://git.suckless.org/surf $SL/surf
 git clone https://github.com/dargzero/okki-status $SL/okki-status
 
 echo linking sl configs...
 ln -sf $BASEDIR/dwm/config.h $SL/dwm/config.h
 ln -sf $BASEDIR/dmenu/config.h $SL/dmenu/config.h
 ln -sf $BASEDIR/st/config.h $SL/st/config.h
-ln -sf $BASEDIR/surf/config.h $SL/surf/config.h
 
-echo ready for manual steps.
+echo "please proceed with manual steps:"
+echo "  - set fish as default shell"
+echo "  - patch and build sl apps"
+echo "  - setup stig credentials"
