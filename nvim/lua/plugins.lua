8e41463d0c0f74e385230d16727a8e67b93e7d31
Richard Garai <garric@okki.hu>
2022-03-08 22:05:48 +0100

    nvim ctrlp

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -15,6 +15,7 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 Plug('preservim/nerdtree')      -- file browser
 Plug('itchyny/lightline.vim')   -- powerline
 Plug('tpope/vim-fugitive')      -- git helper
+Plug('ctrlpvim/ctrlp.vim')      -- finder
 Plug('EdenEast/nightfox.nvim')  -- color theme
 
 vim.call('plug#end')
